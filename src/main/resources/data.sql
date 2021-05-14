bc6401f36e611de4be85d8867e4664327624b089
Kuruegane <ghfb145@gmail.com>
2021-05-14 19:38:25 -0300

    H2 Database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into contato ( nome, tipo, telefone) values ( 'Fulano', 'Residencial', '1234-1234')
+insert into contato ( nome, tipo, telefone) values ( 'Beltrano', 'Comercial', '9876-5432')
