3a558e8fee886dd055a0f35f1e7bd90139d607c0
Kuruegane <ghfb145@gmail.com>
2021-05-17 17:33:42 -0300

    H2 Insert new contato

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert into contato ( nome, tipo, telefone) values ( 'Fulano', 'Residencial', '1234-1234')
-insert into contato ( nome, tipo, telefone) values ( 'Beltrano', 'Comercial', '9876-5432')
+insert into contato (id, nome, tipo, telefone) values (1, 'Fulano', 'Residencial', '1234-1234')
+insert into contato (id, nome, tipo, telefone) values (2, 'Beltrano', 'Comercial', '9876-5432')
+insert into contato (id, nome, tipo, telefone) values (3, 'Sicrano', 'Residencial', '3334-2234')
