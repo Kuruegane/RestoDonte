be0f3390dc39c9fd0fd047befd90ff9ac86ecb38
Kuruegane <ghfb145@gmail.com>
2021-05-23 17:12:39 -0300

    Deleted Files

--- src/main/java/br/edu/usj/ads/pw/agenda/ContatoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.usj.ads.pw.agenda;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContatoRepository extends CrudRepository<Contato, Long> {
-    List<Contato> findAll();
-}
